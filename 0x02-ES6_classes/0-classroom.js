d7c91a907c6ec9ab0a5999639ffa8d7080c1b12d
belyne <122968087+belyne@users.noreply.github.com>
2024-01-05 22:11:52 +0200

    Update 0-classroom.js

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,9 +1,6 @@
-import ClassRoom from './0-classroom';
-
-export default function initializeRooms() {
-  const class1 = new ClassRoom(19);
-  const class2 = new ClassRoom(20);
-  const class3 = new ClassRoom(34);
-  const newList = [class1, class2, class3];
-  return (newList);
+// 0-classroom.js
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
