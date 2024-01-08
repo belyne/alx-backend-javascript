5b5e778477906e056e41a8869d5ac4945febd0f4
belyne <nzabonamarie@gmail.com>
2024-01-09 01:38:28 +0200

    More mapping

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,9 @@
 // 1-get_list_student_ids.js
 
-export default function getListStudentIds(array) {
-  if (!Array.isArray(array)) {
+export default function getListStudentIds(students) {
+  if (!Array.isArray(students)) {
     return [];
   }
 
-  return array.map(student => student.id);
+  return students.map(student => student.id);
 }
