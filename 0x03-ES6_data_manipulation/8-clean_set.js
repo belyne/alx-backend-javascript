84802a355e762cd2d6337729112929da858d230b
belyne <nzabonamarie@gmail.com>
2024-01-09 02:38:41 +0200

    update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,13 @@
 // 8-clean_set.js
 
-export default function cleanSet(set, startString) {
-  const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
-  return filteredValues.join('-').substring(startString.length);
+export default function cleanSet(inputSet, startString) {
+  if (!(inputSet instanceof Set) || typeof startString !== 'string') {
+    throw new Error('Invalid input. Please provide a Set and a startString.');
+  }
+
+  const filteredValues = Array.from(inputSet)
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length));
+
+  return filteredValues.join('-');
 }
