40c9aac7907fe6a410173168f449191d0a184e62
belyne <nzabonamarie@gmail.com>
2024-01-09 02:32:51 +0200

    update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 // 8-clean_set.js
 
 export default function cleanSet(set, startString) {
-  const filteredValues = Array.from(set).filter(value => value.startsWith(startString));
+  const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
   return filteredValues.join('-').substring(startString.length);
 }
