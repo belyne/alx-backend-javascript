d5c8a2007c2d92c352426822c523eb74d9e896d9
belyne <nzabonamarie@gmail.com>
2024-01-09 02:30:44 +0200

    update 7-has_array_values.js

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,5 +1,5 @@
 // 7-has_array_values.js
 
 export default function hasValuesFromArray(set, array) {
-  return array.every(value => set.has(value));
+  return array.every((value) => set.has(value));
 }
