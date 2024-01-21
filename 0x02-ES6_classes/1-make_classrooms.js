a5ad1b215dcb7a35663ce3545ae2a7729b09eea3
belyne <nzabonamarie@gmail.com>
2024-01-21 18:48:58 +0000

    classes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,13 +1,9 @@
-// 1-make_classrooms.js
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
-function initializeRooms() {
-  const roomSizes = [19, 20, 34];
-
-  // Create an array of ClassRoom objects with specified sizes
-  const rooms = roomSizes.map(size => new ClassRoom(size));
-
-  return rooms;
+export default function initializeRooms() {
+  const class1 = new ClassRoom(19);
+  const class2 = new ClassRoom(20);
+  const class3 = new ClassRoom(34);
+  const newList = [class1, class2, class3];
+  return (newList);
 }
-
-export default initializeRooms;
