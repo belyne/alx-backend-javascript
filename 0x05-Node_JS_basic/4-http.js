aafd2674cd661ccb366c260d31c5f3bdc6613ed3
belyne <122968087+belyne@users.noreply.github.com>
2024-02-28 16:52:09 +0200

    Update 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,20 +2,15 @@
 
 const http = require('http');
 
-// Create an HTTP server
+const host = '127.0.0.1';
+const port = 1245;
+
 const app = http.createServer((req, res) => {
-  // Set the response header
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-
-  // Send the response
-  res.end('Hello Holberton School!\n');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
-// Listen on port 1245
-const PORT = 1245;
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+app.listen(port, host, () => {});
 
-// Export the app variable
 module.exports = app;
