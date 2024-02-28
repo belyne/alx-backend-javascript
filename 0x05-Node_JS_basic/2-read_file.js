511ccac71d774a9a23e37b19ab7c105b32e715ec
belyne <122968087+belyne@users.noreply.github.com>
2024-02-28 16:47:53 +0200

    Update 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -2,31 +2,38 @@
 
 const fs = require('fs');
 
-function countStudents(path) {
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let length = 0;
   try {
-    // Read the database file synchronously
-    const data = fs.readFileSync(path, 'utf8');
-    
-    // Split the data into lines
-    const lines = data.split('\n').filter(line => line.trim() !== '');
-
-    // Extract and count students in each field
-    const students = {};
-    lines.forEach(line => {
-      const [firstName, , , field] = line.split(',');
-      if (!students[field]) {
-        students[field] = [];
+    const content = fs.readFileSync(fileName, 'utf-8');
+    const lines = content.toString().split('\n');
+    for (let i = 0; i < lines.length; i += 1) {
+      if (lines[i]) {
+        length += 1;
+        const field = lines[i].toString().split(',');
+        if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+          students[field[3]].push(field[0]);
+        } else {
+          students[field[3]] = [field[0]];
+        }
+        if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+          fields[field[3]] += 1;
+        } else {
+          fields[field[3]] = 1;
+        }
       }
-      students[field].push(firstName.trim());
-    });
-
-    // Log the number of students in each field
-    console.log(`Number of students: ${lines.length}`);
-    Object.entries(students).forEach(([field, names]) => {
-      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
-    });
+    }
+    const l = length - 1;
+    console.log(`Number of students: ${l}`);
+    for (const [key, value] of Object.entries(fields)) {
+      if (key !== 'field') {
+        console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
+      }
+    }
   } catch (error) {
-    throw new Error('Cannot load the database');
+    throw Error('Cannot load the database');
   }
 }
 
