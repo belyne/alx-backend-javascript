5f720f929df1a18972956747dc6a1e401a917f37
belyne <122968087+belyne@users.noreply.github.com>
2024-02-28 16:46:33 +0200

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,20 +2,17 @@
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-
-  if (name) {
-    console.log(`Your name is: ${name}`);
-  }
-
-  process.stdout.write('This important software is now closing\n');
-  process.exit();
-});
-
-process.on('SIGINT', () => {
-  process.stdout.write('This important software is now closing\n');
-  process.exit();
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
