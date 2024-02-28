758a773d90c814023ea381767cca09b6df341e9d
belyne <nzabonamarie@gamil.com>
2024-02-28 16:28:54 +0000

    updating full_server/*.js

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -5,10 +5,10 @@ const fs = require('fs').promises;
 function readDatabase(filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8')
-      .then(data => {
-        const lines = data.split('\n').filter(line => line.trim() !== '');
+      .then((data) => {
+        const lines = data.split('\n').filter((line) => line.trim() !== '');
         const students = {};
-        lines.forEach(line => {
+        lines.forEach((line) => {
           const [firstName, , , field] = line.split(',');
           if (!students[field]) {
             students[field] = [];
@@ -17,7 +17,7 @@ function readDatabase(filePath) {
         });
         resolve(students);
       })
-      .catch(error => reject(new Error('Cannot load the database')));
+      .catch((error) => reject(new Error('Cannot load the database')));
   });
 }
 
