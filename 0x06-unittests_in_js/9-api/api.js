fb77790778b240ba021ff67193f8705baf558041
belyne <122968087+belyne@users.noreply.github.com>
2024-03-04 20:21:04 +0200

    Create api.js

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,21 @@
+// 9-api/api.js
+const express = require('express');
+const app = express();
+
+const PORT = 7865;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+// New endpoint with regex validation for :id (must be only a number)
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const { id } = req.params;
+  res.send(`Payment methods for cart ${id}`);
+});
+
+const server = app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
+});
+
+module.exports = server;
