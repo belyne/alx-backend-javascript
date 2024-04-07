b4a890add60fd49b9fa676e0ef5b5f9eb57a185a
belyne <122968087+belyne@users.noreply.github.com>
2024-04-07 10:50:42 +0200

    Update 5-payment.test.js

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,34 +1,24 @@
-// 5-payment.test.js
 const sinon = require('sinon');
-const { expect } = require('chai');
-const sendPaymentRequestToApi = require('./5-payment');
+const sendPaymentRequestToApi = require('./5-payment.js');
 
-describe('sendPaymentRequestToApi', () => {
-  let consoleLogSpy;
+describe('sendPaymentRequestToApi', function () {
+  let SSpy;
 
-  beforeEach(() => {
-    // Create a spy for console.log before each test
-    consoleLogSpy = sinon.spy(console, 'log');
+  beforeEach(function () {
+    SSpy = sinon.spy(console, 'log');
   });
 
-  afterEach(() => {
-    // Restore the spy after each test
-    consoleLogSpy.restore();
+  afterEach(function () {
+    SSpy.restore();
   });
 
-  it('should log the correct message for totalAmount = 100, totalShipping = 20', () => {
-    // Call the function
+  it('should log "The total is: 120" for inputs 100 and 20', function () {
     sendPaymentRequestToApi(100, 20);
-
-    // Assert that console.log was called with the correct message
-    expect(consoleLogSpy.calledOnceWithExactly('The total is: 120')).to.be.true;
+    sinon.assert.calledOnceWithExactly(SSpy, 'The total is: 120');
   });
 
-  it('should log the correct message for totalAmount = 10, totalShipping = 10', () => {
-    // Call the function
+  it('should log "The total is: 20" for inputs 10 and 10', function () {
     sendPaymentRequestToApi(10, 10);
-
-    // Assert that console.log was called with the correct message
-    expect(consoleLogSpy.calledOnceWithExactly('The total is: 20')).to.be.true;
+    sinon.assert.calledOnceWithExactly(SSpy, 'The total is: 20');
   });
 });
