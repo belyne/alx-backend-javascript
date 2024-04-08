a907ec5ae328cb2d8c3f8ddf57f3e6801bba3841
belyne <122968087+belyne@users.noreply.github.com>
2024-04-08 10:02:59 +0200

    Delete 0x06-unittests_in_js/utils-0.js

--- 0x06-unittests_in_js/utils-0.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  calculateNumber(type, a, b) {
-    if (type === 'SUM') return a + b;
-    if (type === 'SUBTRACT') return a - b;
-    if (type === 'DIVIDE') return b === 0 ? 'Error' : a / b;
-  }
-};
